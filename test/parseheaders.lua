cb472a3a64c142733aa144df0ec37953cad3124f
Takehiro Iyatomi <iyatomi@dokyogames.com>
2014-02-04 06:24:33 +0900

    fix indent && update parse setting

--- test/parseheaders.lua
+++ test/parseheaders.lua
@@ -15,39 +15,40 @@ if ffi.os == "OSX" then
                 "ucontext.h", -- The deprecated ucontext routines require _XOPEN_SOURCE to be defined
         }
 elseif ffi.os == "Linux" then
-        blacklist = {
-                "complex.h", -- luajit itself will support for it
+    blacklist = {
+        "complex.h", -- luajit itself will support for it
 		"cursesapp.h", "cursesf.h", "cursesm.h", "cursesp.h", "cursesw.h", "cursslk.h", -- c++ file
-       		"etip.h", -- c++ file
-            "link.h", -- missing Elf__ELF_NATIVE_CLASS_Addr
-	 }
+       	"autosprintf.h", "etip.h", -- c++ file
+        "link.h", -- missing Elf__ELF_NATIVE_CLASS_Addr
+	}
 end
+arg[1] = "driver.h"
 while true do
-        local file = dir:read()
-        if not file then break end
+    local file = dir:read()
+    if not file then break end
 	if not arg[1] or (arg[1] == file) then
-	if file:find('^[^_]+.*%.h$') then
-                -- print('code:', ('(require "ffiex.init").cdef "#include <%s>"'):format(file))
-                local black
-                if blacklist then
-                        for _,fn in ipairs(blacklist) do
-                                if fn == file then
-                                        black = true
-                                end
-                        end
+    	if file:find('^[^_]+.*%.h$') then
+            -- print('code:', ('(require "ffiex.init").cdef "#include <%s>"'):format(file))
+            local black
+            if blacklist then
+                for _,fn in ipairs(blacklist) do
+                    if fn == file then
+                        black = true
+                    end
                 end
-                if not black then
-                        print('try parse:' .. file)
-                        local ok, r = pcall(os.execute, ('luajit test/parser.lua_ %s'):format(file))
-                        --local ok, r = pcall(loadstring, ('(require "ffiex.init").cdef "#include <%s>"'):format(file))
-                        if ok and r == 0 then
-                        else
-                                print('parse fails:' .. file .. '|' .. tostring(r))
-                                os.exit(-2)
-                        end
+            end
+            if not black then
+                print('try parse:' .. file)
+                local ok, r = pcall(os.execute, ('luajit test/parser.lua_ %s'):format(file))
+                --local ok, r = pcall(loadstring, ('(require "ffiex.init").cdef "#include <%s>"'):format(file))
+                if ok and r == 0 then
+                else
+                    print('parse fails:' .. file .. '|' .. tostring(r))
+                    os.exit(-2)
                 end
+            end
         else
-                print('not test:' .. file)
+            print('not test:' .. file)
         end
 	end
 end
